a64066fde82083331f80088efd649c9eb3cbfa57
yassinemiladi21 <yassinemiladi21@gmail.com>
2022-06-27 19:43:22 +0100

    update login page

--- loginregister/login.php
+++ loginregister/login.php
@@ -8,28 +8,43 @@
     <link rel="stylesheet" href="style.css" />
   </head> 
   <body>
+    <div class="container">
+    <div class="desc">
+      <div>
     <h1 class="title"><i>FindIT</i></h1>
+    <p>FindIT is a platform that connects individuals and/or professionals who publish an ad made up of text and photos to offer the sale of new or second-hand products, a rental or a service in exchange of compensation.</p>
+     </div>
+    <div class="image"><img src="img/bg.png" alt="" class="img-responsive"></div>
+    <!--
+    <div class="images">
+      <div class="image"><img src="img/img1.png" alt="" class="img-responsive"></div>
+      <div class="image"><img src="img/img2.png" alt="" class="img-responsive"></div>
+      <div class="image"><img src="img/img3.png" alt="" class="img-responsive"></div>
+
+    </div> -->
+  </div>
     <div class="login" style="z-index: 1; opacity: 1">
       <h1>LOGIN</h1>
       <form method="POST">
         <div class="text">
-          <input type="number" id="user" name="usern" required />
+          <input type="text" id="user" name="user" required />
           <span></span>
           <label for="user">Username</label>
         </div>
         <div class="text">
-          <input type="password" name="pwd" id="pass" required />
+          <input type="password" name="pwd" id="pwd" required />
           <span></span>
-          <label for="pass">Password</label>
+          <label for="pwd">Password</label>
         </div>
-        <input type="submit" name="Submit1" value="Connect"/>
+        <input type="submit" name="submit" value="Connect"/>
 
         <div class="signup-text">
           Not a member ? <a href="#" onclick="change1()">Sign Up</a>
         </div>
       </form>
     </div>
-
+    </div>
+<!--
      <div class="signup" style="z-index: 0; opacity: 0">
       <h1>S'inscrire</h1>
       <form action="send.php" method="post">
@@ -69,7 +84,7 @@
           Deja un membre ? <a href="#" onclick="change2()">Se connecter</a>
         </div>
       </form>
-    </div>
+    </div> -->
 
     <script src="insc.js"></script>
   </body>
